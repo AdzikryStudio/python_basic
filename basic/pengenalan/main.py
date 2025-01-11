4ae1a1032326c9159f7d3c59b585ea95c14bb203
Muhammad Faqih <faqihbelajarid@gmail.com>
2025-01-11 18:40:04 +0700

    Add files via upload

---
+++ basic/pengenalan/main.py
@@ -0,0 +1,12 @@
+import time
+start_time = time.time()
+
+print ("HELLO")
+print ("HELLO")
+print ("HELLO SAYA")
+
+print ("HELLO MINE")
+#COOMENT
+""" COMMENT MULTILINE"""
+
+print(time.time() - start_time, "detik")
