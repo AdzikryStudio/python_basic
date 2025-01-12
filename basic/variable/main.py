fbaf1ecd4763e7a2a8f6defdd2a59c14af235c6b
Muhammad Faqih <faqihbelajarid@gmail.com>
2025-01-12 19:20:08 +0700

    Add files via upload

---
+++ basic/variable/main.py
@@ -0,0 +1,26 @@
+# variael adalah tempat menyimpan data
+
+# assigment value
+x = 10
+y = 5
+
+z = 1000
+
+# panggil
+print("nilai x = ", x)
+print("nilai y = ", y)
+print("nilai z = ", z)
+
+# penamman
+# undersore, atau value di belakang
+nilai_y = 15
+million10 = 10000000000
+nilai_z = 1888
+
+# pemanggilan 2
+print("nilai x = ", x)
+x = 17
+print("nilai x = ", x)
+
+y = x
+print("nilai x = ", x)
