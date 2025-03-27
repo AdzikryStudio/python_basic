4a192c430ebb7414cfb98ba7e5422c0ac7978df4
Muhammad Faqih <faqihbelajarid@gmail.com>
2025-03-27 12:50:30 +0700

    Update main.py

--- basic/input_user/main.py
+++ basic/input_user/main.py
@@ -1,1 +1,17 @@
-# input from user
+# input form user
+
+# data string selalu
+data = input("Masukkan Nama : ")
+print("data : ", data,"type = ", type(data))
+
+# jika int
+angka = float(input("Masukkan Angka Desimal: "))
+angka = int(input("Masukkan Angka: "))
+
+print("angka : ", angka,"type = ", type(angka))
+
+# jika bool
+
+biner = bool(int(input("Masukkan Nilai: ")))
+
+print("biner : ", biner,"type = ", type(biner))
